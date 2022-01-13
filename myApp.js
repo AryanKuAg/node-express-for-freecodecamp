edd62e7c1079fcc17d7a291bfd1e614093969c63
Aryan Agrawal <alemantrixaryan@gmail.com>
2022-01-13 23:53:44 +0530

    absolute path added for views/index.html

--- myApp.js
+++ myApp.js
@@ -1,12 +1,15 @@
 var express = require('express');
+var path = require('path')
 var app = express();
+
 
 
 console.log("Hello World")
 
 
 app.get("/", (req, res) => {
-    res.send("Hello Express")
+    abspath = path.join(__dirname, 'views/index.html')
+    res.sendFile(abspath)
 })
 
 
